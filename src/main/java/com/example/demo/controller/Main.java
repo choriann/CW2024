2533cb5b828974f41e53cba4ca57345c4a309bdb
Cheah Chor Ian <choriancheah@gmail.com>
2024-12-12 00:55:58 +0800

    Made the screen maximise upon launching the game, improving the game experience

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -6,16 +6,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static final int SCREEN_WIDTH = 1300;
-	private static final int SCREEN_HEIGHT = 750;
+
 	private static final String TITLE = "Sky Battle";
 
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle(TITLE);
-		stage.setResizable(false);
-		stage.setHeight(SCREEN_HEIGHT);
-		stage.setWidth(SCREEN_WIDTH);
+		stage.setResizable(true);
+
+		stage.setMaximized(true);
 
 		Controller controller = new Controller(stage);
 		MainMenu mainMenu = new MainMenu(stage, controller);
