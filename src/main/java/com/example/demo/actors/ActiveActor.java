9ccd731c0a89becbbcc8c421f47b100db012771b
Cheah Chor Ian <choriancheah@gmail.com>
2024-12-13 17:22:47 +0800

    Fixed invocation to toExternalForm() that may produce NullPointerException by adapting code to use Objects.requireNonNull and also added Javadoc comments.

--- src/main/java/com/example/demo/actors/ActiveActor.java
+++ src/main/java/com/example/demo/actors/ActiveActor.java
@@ -1,28 +1,49 @@
 package com.example.demo.actors;
 
+import java.util.Objects;
 import javafx.scene.image.*;
 
+/**
+ * Abstract class representing an active actor in the game.
+ */
 public abstract class ActiveActor extends ImageView {
-	
+
 	private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
 	public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		//this.setImage(new Image(IMAGE_LOCATION + imageName));
-		this.setImage(new Image(getClass().getResource(IMAGE_LOCATION + imageName).toExternalForm()));
+		// Ensure the image path is valid and non-null
+		String imagePath = Objects.requireNonNull(
+				getClass().getResource(IMAGE_LOCATION + imageName),
+				"Image resource not found: " + IMAGE_LOCATION + imageName
+		).toExternalForm();
+
+		this.setImage(new Image(imagePath));
 		this.setLayoutX(initialXPos);
 		this.setLayoutY(initialYPos);
 		this.setFitHeight(imageHeight);
 		this.setPreserveRatio(true);
 	}
 
+	/**
+	 * Updates the position of the actor.
+	 */
 	public abstract void updatePosition();
 
+	/**
+	 * Moves the actor horizontally by a specified amount.
+	 *
+	 * @param horizontalMove the amount to move horizontally
+	 */
 	protected void moveHorizontally(double horizontalMove) {
 		this.setTranslateX(getTranslateX() + horizontalMove);
 	}
 
+	/**
+	 * Moves the actor vertically by a specified amount.
+	 *
+	 * @param verticalMove the amount to move vertically
+	 */
 	protected void moveVertically(double verticalMove) {
 		this.setTranslateY(getTranslateY() + verticalMove);
 	}
-
 }
